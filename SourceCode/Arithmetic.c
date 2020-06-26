410ae80ed250c3e48bff0d4cc4fd0401fa144002
Mohit Gupta <mohitguptamt123@gmail.com>
2020-06-26 11:38:54 +0530

    made 2 new directories: Maths and Experiments 	moved files: Arithmetic.c and Hello.c 	new file: Maths/Algebra.c

--- SourceCode/Arithmetic.c
+++
@@ -1,7 +0,0 @@
-int sum(int x, int y){
-	return x + y;
-}
-
-int sub(int x, int y){
-	return x - y;
-}
---
+++ SourceCode/Maths/Arithmetic.c
@@ -0,0 +1,15 @@
+int sum(int x, int y){
+	return x + y;
+}
+
+int sub(int x, int y){
+	return x - y;
+}
+
+float div(int x, int y) {
+	return x/y;
+}
+
+float mul (int x, int y) {
+	return x*y;
+}
